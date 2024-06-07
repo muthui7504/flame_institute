3a1383151734086c167fac4d6b823111422cbd5f
muthui7504 <muthui7504@gmail.com>
2024-06-07 09:34:11 +0300

    new changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,24 +1,158 @@
-import React from 'react'
-import './Hero.css'
-import { FaArrowRight } from "react-icons/fa6";
-import { Link } from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
+import './Hero.css';
+import landscape from '../../assets/landscape.jpg';
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { FaArrowCircleRight } from "react-icons/fa";
 
-const Hero = () => {
+
+function Hero() {
+  const nextRef = useRef(null);
+  const prevRef = useRef(null);
+  const carouselRef = useRef(null);
+  const thumbnailBorderRef = useRef(null);
+  const timeRef = useRef(null);
+
+  useEffect(() => {
+    const nextDom = nextRef.current;
+    const prevDom = prevRef.current;
+    const carouselDom = carouselRef.current;
+    const thumbnailBorderDom = thumbnailBorderRef.current;
+    const thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+    const SliderDom = carouselDom.querySelector('.list');
+
+    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+
+    let timeRunning = 3000;
+    let timeAutoNext = 7000;
+
+    const showSlider = (type) => {
+      const SliderItemsDom = SliderDom.querySelectorAll('.item');
+      const thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+
+      if (type === 'next') {
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+      } else {
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+      }
+
+      clearTimeout(runTimeOut);
+      runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+      }, timeRunning);
+
+      clearTimeout(runNextAuto);
+      runNextAuto = setTimeout(() => {
+        nextDom.click();
+      }, timeAutoNext);
+    };
+
+    nextDom.onclick = () => showSlider('next');
+    prevDom.onclick = () => showSlider('prev');
+
+    let runTimeOut;
+    let runNextAuto = setTimeout(() => {
+      nextDom.click();
+    }, timeAutoNext);
+
+    return () => {
+      clearTimeout(runTimeOut);
+      clearTimeout(runNextAuto);
+    };
+  }, []);
+
   return (
-    <div className='hero container'>
-       <div className="hero-text">
-        <h1>Flame Institute and Therapy</h1>
-        <h3>Mission</h3>
-        <p>To Impact Society through Counselling, Training, Mentorship, Consultancy and Research</p>
-        <h3>Vision</h3>
-        <p>A Global-standards, Generational- relevant,     Community-centred Training and Therapy Institute.</p>
-        <h3>Core Values</h3>
-        <p>
-        <span className="hover-word" data-definition="A belief that the Family is the cornerstone of any society and has to be taken care of to have any progress and stability.">Family</span>, <span className="hover-word" data-definition="Society falls and rises with leadership, thus stressing on strategic, servant, and spiritual leadership. ">Leadership</span> <span className="hover-word" data-definition="Education is the pedestal on which greatness is attained and should be a route towards other attainments in the economy including entrepreneurship, innovation, sports and health. ">Academics</span> <span className="hover-word" data-definition=" Church and the subsequent service attached to it are key balancing and stabilizing factors of any society, without which morals and ethics find no place of exercise.">Ministry</span>        <span className="hover-word" >and</span>! <span className="hover-word" data-definition="The state or quality of being excellent">Empowerment</span>
-    </p>
-       </div>
+    <div>
+      <div className="carousel" ref={carouselRef}>
+        {/*-- list item --*/}
+        <div className="list">
+          <div className="item">
+            <img src={landscape} alt="landscape" />
+            <div className="content">
+              <div className="topic">ABOUT</div>
+              <div className="des">
+                {/*-- lorem 50 --*/}
+                Flame Institute and Therapy is a Multifaceted agency offering tailored mentorship,counseling,leadership, training & spiritual services
+              </div>
+              
+            </div>
+          </div>
+          <div className="item">
+            <img src={landscape} alt="landscape" />
+            <div className="content">
+              <div className="topic">VISION</div>
+              <div className="des">
+                A Global-standard, Generational-relevant, Community centered Training and Therapy Institute.
+              </div>``
+              
+            </div>
+          </div>
+          <div className="item">
+            <img src={landscape} alt="landscape" />
+            <div className="content">
+              <div className="topic">MISSION</div>
+              <div className="des">
+                To Impact Society through Counselling, Training, Mentorship, Consultancy and Reserch
+              </div>
+              
+            </div>
+          </div>
+          <div className="item">
+            <img src={landscape} alt="landscape" />
+            <div className="content">
+              <div className="topic">CORE VALUES</div>
+              <div className="des">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        {/* -- list thumbnail -- */}
+        <div className="thumbnail" ref={thumbnailBorderRef}>
+          <div className="item">
+            <img src={landscape} alt="landscape" />
+            <div className="content">
+              <div className="title">VISSION</div>
+              <div className="description">Description</div>
+            </div>
+          </div>
+          <div className="item">
+            <img src={landscape} alt="landscape" />
+            <div className="content">
+              <div className="title">MISSION</div>
+              <div className="description">Description</div>
+            </div>
+          </div>
+          <div className="item">
+            <img src={landscape} alt="landscape" />
+            <div className="content">
+              <div className="title">CORE VALUES</div>
+              <div className="description">Description</div>
+            </div>
+          </div>
+          <div className="item">
+            <img src={landscape} alt="landscape" />
+            <div className="content">
+              <div className="title">ABOUT</div>
+              <div className="description">Description</div>
+            </div>
+          </div>
+        </div>
+        {/* -- next prev -- */}
+        <div className="arrows">
+          <button id="prev" ref={prevRef}><FaArrowCircleLeft /></button>
+          <button id="next" ref={nextRef}><FaArrowCircleRight /></button>
+        </div>
+        {/* -- time running -- */}
+        <div className="time" ref={timeRef}></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
